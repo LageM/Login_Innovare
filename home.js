f0f924e2142ca8d39b915c87d46dab10d7af3c33
LageM <112203095+LageM@users.noreply.github.com>
2025-12-25 18:22:13 -0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,21 @@
+const usuario = JSON.parse(localStorage.getItem("usuario_logado") || "null");
+
+if (!usuario) {
+  window.location.href = "login.html";
+}
+
+document.getElementById("hello").textContent = `Olá, ${usuario.nome}!`;
+document.getElementById("secao").textContent = `Seção: ${usuario.secao}`;
+
+// menu mobile
+const sidebar = document.getElementById("sidebar");
+document.getElementById("btnMenu").addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+});
+
+// logout
+document.getElementById("btnLogout").addEventListener("click", () => {
+  localStorage.removeItem("usuario_logado");
+  // mantém login_saved se você quiser continuar com autologin
+  window.location.href = "login.html";
+});
